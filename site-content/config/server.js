812f802cf4131915383742886a10d8b391ec1aac
coelhodiana <coelhodiana04@gmail.com>
2023-03-28 23:13:24 -0400

    config: server

--- site-content/config/server.js
+++ site-content/config/server.js
@@ -7,4 +7,17 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  url: env('URL', env.API),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'seu_jwt_secreto'),
+    },
+    url: env('ADMIN_URL', `${env.API}/admin`),
+  },
+  ssl: {
+    enabled: true,
+    key: `/etc/letsencrypt/live/${env.API}/privkey.pem`,
+    cert: `/etc/letsencrypt/live/${env.API}/fullchain.pem`,
+  },
 });
+
